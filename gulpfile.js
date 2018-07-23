69379e97ee68706f32f62b0c0832f5c74d92817b
vzaffalon <zaffalonvictor@gmail.com>
2018-07-23 03:10:36 -0300

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -24,10 +24,10 @@ gulp.task('default', ['clean'], function () {
   gulp.start('build');
 });
 
-gulp.task('serveprod', function() {
+gulp.task('serveprod',['build'], function() {
   return gulp.src('./release/').pipe(webserver({
       port: process.env.PORT || 5000,
-      https: false,
+      https: true,
       open: true
   }));
 });
