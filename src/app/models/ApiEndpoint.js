5d70bba0ce8c64747e10a07a1b443aa7b5fbf289
vzaffalon <zaffalonvictor@gmail.com>
2018-07-23 03:47:35 -0300

    connect to heroku webservice

--- src/app/models/ApiEndpoint.js
+++ src/app/models/ApiEndpoint.js
@@ -1,5 +1,6 @@
 'use strict';
 
 angular.module('RockDroid.pages').factory('ApiEndpoint', function () {
-    return "http://localhost:3000";
+    // return "http://localhost:3000";
+    return "https://rockdroidwebservice.herokuapp.com/";
 });
