9b0ed62ae9fd4e3460557291ea9e6013ea5c011a
vzaffalon <zaffalonvictor@gmail.com>
2018-07-23 01:44:15 -0300

    proc

--- gulpfile.js
+++ gulpfile.js
@@ -21,6 +21,7 @@ wrench.readdirSyncRecursive('./gulp').filter(function(file) {
  */
 gulp.task('default', ['clean'], function () {
   gulp.start('build');
+  gulp.start('serveprod');
 });
 
 gulp.task('serveprod',['default'], function() {
