ef820978324fe2757b836786281302121f460f2e
vzaffalon <zaffalonvictor@gmail.com>
2018-07-23 03:06:36 -0300

    oficial deploy config

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 var gulp = require('gulp');
 var wrench = require('wrench');
 var connect = require('gulp-connect')
+var webserver = require('gulp-webserver');
 
 /**
  *  This will load all js or coffee files in the gulp directory
@@ -23,10 +24,10 @@ gulp.task('default', ['clean'], function () {
   gulp.start('build');
 });
 
-gulp.task('serveprod',[], function() {
-  connect.server({
-    root: 'release',
-    port: process.env.PORT || 5000, // localhost:5000
-    livereload: false
-  });
+gulp.task('serveprod', function() {
+  return gulp.src('./release/').pipe(webserver({
+      port: process.env.PORT || 5000,
+      https: false,
+      open: true
+  }));
 });
